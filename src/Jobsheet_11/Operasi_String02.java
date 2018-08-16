6937e6b4b9053935cec9bf96d7e7eee8a84fdc5e
Owner <Owner@DESKTOP-3762CMA>
2018-08-16 22:35:10 +0700

    Initial Commit

---
+++ src/Jobsheet_11/Operasi_String02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet_11;
+import java.util.Scanner;
+public class Operasi_String02 {
+     private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    
+    public static void main(String[] args){
+    
+        String identitas = " Rafi Raihan Firdaus / X RPL 2 / 26 ";
+        
+        tampilJudul(identitas); 
+    }
+  
+    }
+
+
+
+
+    
+
